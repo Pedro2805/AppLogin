4ba9ec4e5d4bb8075d3906519094aec82396f140
Pedro Mota <pedromota.3339@aluno.saojuas.br>
2020-09-29 12:10:29 -0300

    Initial commit

---
+++ app/src/main/java/br/com/pedroteste/applogin/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.pedroteste.applogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
